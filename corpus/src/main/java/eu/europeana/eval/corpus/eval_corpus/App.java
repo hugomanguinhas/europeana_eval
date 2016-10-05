8e2a313cdba391dc803667770006bc2591378fe1
Hugo Manguinhas <hugo.manguinhas@europeana.eu>
2016-10-05 14:55:22 +0200

    Master

---
+++ corpus/src/main/java/eu/europeana/eval/corpus/eval_corpus/App.java
@@ -0,0 +1,13 @@
+package eu.europeana.eval.corpus.eval_corpus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
